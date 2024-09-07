ec11e4e7e294942ab72e24fe5eb3710e8a4510e4
Leslie Claussen <leslie.claussen@mail.com>
2024-09-07 21:28:50 +0200

    7th commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const elementFaq = document.querySelectorAll(".faq__element__header h2");
+const faqHeaders = document.querySelectorAll(".faq__element__header h2");
+
+function toggleFAQ(Header) {
+  console.log("clicked FAQ", Header.textContent);
+  Header.parentNode.nextElementSibling.classList.toggle("active");
+
+  const imageElement = 
+}
+
+faqHeaders.forEach((Header) => {
+  Header.addEventListener("click", () => {
+    console.log("click");
+    toggleFAQ(Header);
+  });
+});
